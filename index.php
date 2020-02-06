6c3ffc0360f4393dbf5927ae910402e26c5f6285
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-06 15:56:58 +0100

    index.php première requete sql en php !!!
    +import database MCU

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
  *  Author: Samuel Roland
  *  Creation date: 06.02.2020
  */
+$user = "ICT-151";
+$pass = "Pa\$\$w0rd";
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=mcu', $user, $pass);
+    foreach ($dbh->query('SELECT * from actors') as $row) {
+        var_dump($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 
 
 ?>
