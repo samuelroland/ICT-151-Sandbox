45ad8c3a7d7a3a37c533908efadfa8a915ebe4e7
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-07 09:19:58 +0100

    getAllItems fin et tests unitaire associé

--- index.php
+++ index.php
@@ -5,18 +5,31 @@
  *  Author: Samuel Roland
  *  Creation date: 06.02.2020
  */
-require_once '.const.php';
 
-try {
-    $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
-    foreach ($dbh->query('SELECT * from filmmakers') as $row) {
-        print_r($row);
+
+function getAllItems()  //prendre tous les éléments
+{
+    require '.const.php';  //récuperer les identifiants
+    try {   //essayer
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);   //créer un objet PDO
+$query = "SELECT filmmakersnumber, lastname, firstname FROM filmmakers";//Ecrire la requête.
+        $statment = $dbh->prepare($query);  //préparer la requête
+        $statment->execute();   //éxecuter la requête
+        $queryResult = $statment->fetchAll();   //aller chercher le résultat
+        $dbh = null;    //remettre à zéro
+        return $queryResult;    //retourner le résultat
+    } catch (PDOException $e) { //en cas d'erreur dans le try
+        echo "Error!: " . $e->getMessage() . "<br/>";
+        return null;
     }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
 }
 
 
+//Test unitaire de la fonction getAllItems:
+$items = getAllItems();
+if (count($items) == 4) {
+    echo "getAllItems() retourne 4 éléments OK !!";
+} else {
+    echo "getAllItems() retourne null BUG ...";
+}
 ?>
