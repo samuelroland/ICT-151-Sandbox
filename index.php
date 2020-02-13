9069deca9086d24be30b696f394581e09f6b3535
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-13 16:13:46 +0100

    index fonction et test filmaker

--- index.php
+++ index.php
@@ -28,6 +28,23 @@ function getAllItems()  //prendre tous les éléments
     }
 }
 
+function getFilmMakerByName($lastname)
+{
+    require '.const.php';  //récuperer les identifiants
+    try {   //essayer
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);   //créer un objet PDO
+        $query = "SELECT * FROM filmmakers WHERE lastname = '" . $lastname . "';";//Ecrire la requête.
+        $statment = $dbh->prepare($query);  //préparer la requête
+        $statment->execute();   //éxecuter la requête
+        $queryResult = $statment->fetch(PDO::FETCH_ASSOC);   //aller chercher le résultat
+        $dbh = null;    //remettre à zéro
+        return $queryResult;    //retourner le résultat
+    } catch (PDOException $e) { //en cas d'erreur dans le try
+        echo "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+}
+
 function makerOf($filmname)
 {    //trouver le réalisateur d'un film:
     require '.const.php';
@@ -57,16 +74,41 @@ if (count($items) == 4) {
 } else {
     echo "getAllItems() retourne null BUG ...";
 }
-echo "<br/>";
 
-echo "Test unitaire de la fonction getAllItems:";
+echo "<br/>";
 $filmname = "Ant-Man";
 $filmmakers = makerOf($filmname);
+echo "Test unitaire de la fonction getAllItems: Quel sont les réalisateurs de $filmname ??  \n";
+
 if ($filmmakers[0]['firstname'] == "Jean-Philippe" && $filmmakers[2]['firstname'] == "Jean-Michel") {
     echo "makerOf() retourne Jean-Philippe et Jean-Michel donc OK !!";
 } else {
     echo "makerOf() retourne null donc BUG ...";
 }
+echo "\n";
+//Test écrit par M. Carrel (exercie de TDD.)
+echo "Test unitaire de la fonction getFilmMakerName : ";
+$item = getFilmMakerByName('Chamblon');
+print_r($item);
+if ($item['id'] == 3) {
+    echo 'OK !!!';
+} else {
+    echo '### BUG ###';
+}
+echo "\n";
 
+echo "Test unitaire de la fonction updateFilmMaker : ";
+$item = getFilmMakerByName('Chamblon');
+$id = $item['id']; // se souvenir de l'id pour comparer
+$item['firstname'] = 'Gérard';
+$item['lastname'] = 'Menfain';
+updateFilmMaker($item);
+$readback = getFilmMaker($id);
+if (($readback['firstname'] == 'Gérard') && ($readback['lastname'] == 'Menfain')) {
+    echo 'OK !!!';
+} else {
+    echo '### BUG ###';
+}
+echo "\n";
 
 ?>
