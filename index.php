f422e0fe0bd35742c22b5163bfcf774295c12960
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-14 09:43:54 +0100

    index updatefilmmaker

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require '.const.php';  //récuperer les identifiants
 $cmd = "mysql -u $user -p$pass < Restore-MCU-PO-Final.sql";   //command system pour
 exec($cmd);
 
-function getPDO()   //create the PDO object 
+function getPDO()   //create the PDO object
 {
     require '.const.php';  //récuperer les identifiants
     return new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);   //créer un objet PDO
@@ -86,9 +86,52 @@ WHERE films.name = \"Ant-Man\";";
 
 }
 
+function getAllFieldTable($tablename)
+{
+    try {
+        $dbh = getPDO();
+        $query = "SELECT COLUMN_NAME
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA = 'mcu' AND TABLE_NAME = '$tablename';";
+        $statment = $dbh->prepare($query);
+        $statment->execute();
+        $queryResult = $statment->fetchAll(PDO::FETCH_NUM);
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        echo "Error!: " . $e->getMessage() . "\n";
+        return null;
+    }
+}
+
 function updateFilmMaker($filmMaker)
 {
+    $listFields = getAllFieldTable("filmmakers");
+    array_keys($listFields);
+    $listToSet = "";    //liste d'éléments à set formatées
+    foreach ($listFields as $oneField) {
+        if ($oneField != "id") {
+            $listToSet .= $oneField[0] . "=:" . $oneField[0] . ", ";
+        }
+    }
+    $listToSet = substr($listToSet, 0, strlen($listToSet) - 2); //enlever la virgule de fin
 
+    try {
+        $dbh = getPDO();   //créer un objet PDO
+        $query = "UPDATE filmmakers 
+ SET $listToSet
+WHERE id =:id;";//Ecrire la requête.
+
+
+        $statment = $dbh->prepare($query);  //préparer la requête
+        $statment->execute($filmMaker);   //éxecuter la requête
+        $queryResult = $statment->fetch(PDO::FETCH_ASSOC);   //aller chercher le résultat
+        $dbh = null;    //remettre à zéro
+        return $queryResult;    //retourner le résultat
+    } catch (PDOException $e) { //en cas d'erreur dans le try
+        echo "Error!: " . $e->getMessage() . "\n";
+        return null;
+    }
 }
 
 echo "\n";
@@ -125,11 +168,13 @@ echo "\n";
 
 echo "Test unitaire de la fonction updateFilmMaker : ";
 $item = getFilmMakerByName('Chamblon');
+var_dump($item);
 $id = $item['id']; // se souvenir de l'id pour comparer
-$item['firstname'] = 'Gérard';
-$item['lastname'] = 'Menfain';
+$item['firstname'] = 'Gérard';  //changer prénom
+$item['lastname'] = 'Menfain';  //changer nom de famille
 updateFilmMaker($item);
-$readback = getFilmMaker($id);
+$readback = getFilmMaker($id);  //relire après modification
+var_dump($readback);
 if (($readback['firstname'] == 'Gérard') && ($readback['lastname'] == 'Menfain')) {
     echo 'OK !!!';
 } else {
